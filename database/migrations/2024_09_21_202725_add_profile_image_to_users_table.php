8a38b9a98f09a0df3c27284c5aa5941127544d52
hussien shokry <hessinshokry3@gmail.com>
2024-09-22 01:32:13 +0300

    solving migration error

---
+++ database/migrations/2024_09_21_202725_add_profile_image_to_users_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProfileImageToUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('profile_image')->nullable()->after('email'); // Add profile_image column
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('profile_image');
+        });
+    }
+}

b445c75815a5b657927b64e36dcdabca941d1227
hussien shokry <hessinshokry3@gmail.com>
2024-09-25 05:06:51 +0300

    create points table

---
+++ database/migrations/2024_09_25_014307_create_points_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('bus_id'); // Foreign key to the buses table
+            $table->string('name');
+            $table->string('location');
+            $table->text('description')->nullable();
+            $table->time('arrived_time');
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('bus_id')->references('id')->on('buses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+};

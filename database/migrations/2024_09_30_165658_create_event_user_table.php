573e98154753b90d0c1869f7600fa290af9c2ba3
Zeiad Magdy <zeiadmagdy2019@gmail.com>
2024-09-30 20:05:24 +0300

    added migration

---
+++ database/migrations/2024_09_30_165658_create_event_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade'); // Foreign key to events table
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');  // Foreign key to users table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_user');
+    }
+};
